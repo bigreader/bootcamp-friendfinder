23b43b58726ffbe00daac8e28da8c8db1bb282c1
Joshua <bigreaderjd1996@gmail.com>
2019-04-25 22:11:53 -0500

    update friends presets

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,21 @@
 module.exports = [{
-	name: 'Josh',
+	name: 'Josh Dudley',
 	photo: 'http://joshdudley.net/img/photo.jpg',
+	answers: [3, 2, 4, 4, 1, 2, 4, 5, 5, 5]
+}, {
+	name: 'Lord Voldemort',
+	photo: 'http://joshdudley.net/img/photo.jpg',
+	answers: [2, 4, 4, 5, 2, 1, 1, 4, 1, 4]
+}, {
+	name: 'Coraline Jones',
+	photo: 'http://cdn-static.denofgeek.com/sites/denofgeek/files/8/01/coraline-main.jpg',
+	answers: [4, 5, 4, 3, 2, 1, 5, 3, 4, 2]
+}, {
+	name: 'Pompey',
+	photo: 'http://thetypefight.com/images/uploads/letters/TypeFight_5_Sharma.jpg',
 	answers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 }, {
-	name: 'Test',
-	photo: 'http://joshdudley.net/img/photo.jpg',
+	name: 'Simone',
+	photo: 'http://thetypefight.com/images/uploads/letters/TypeFight_1_Eduardo.jpeg',
 	answers: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 }]
