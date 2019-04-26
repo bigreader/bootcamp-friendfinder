f450787710bc3848b0227a78256e41f205697724
Joshua <bigreaderjd1996@gmail.com>
2019-04-25 21:57:05 -0500

    everything

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+module.exports = [{
+	name: 'Josh',
+	photo: 'http://joshdudley.net/img/photo.jpg',
+	answers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+}, {
+	name: 'Test',
+	photo: 'http://joshdudley.net/img/photo.jpg',
+	answers: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+}]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var friends = require('../data/friends.js');
+
+console.log(friends);
 
 var router = express.Router();
 router.use(express.urlencoded({extended: true}));
@@ -9,7 +12,34 @@ router.get('/friends', (req, res) => {
 });
 
 router.post('/friends', (req, res) => {
-	res.json({});
+	var profile = { // clean up the data, who knows what the user sent
+		name: req.body.name,
+		photo: req.body.photo,
+		answers: req.body.answers
+	}
+	console.log(req.body);
+
+	var best = null;
+	var bestScore = Infinity;
+	friends.forEach(friend => {
+		var score = 0;
+		friend.answers.forEach((answer, i) => {
+			score += Math.abs(answer - profile.answers[i]);
+		});
+		if (score < bestScore && friend.name !== profile.name) {
+			bestScore = score;
+			best = friend;
+		}
+	});
+
+	friends.push(profile); // add user to friends database
+	console.log(friends.map(f => f.name));
+
+	res.json({
+		success: (best !== null),
+		score: bestScore,
+		friend: best
+	});
 });
 
 module.exports = router
